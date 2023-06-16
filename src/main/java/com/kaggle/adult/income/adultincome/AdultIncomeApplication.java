b12ff8f7110121fdddc03c6dbc2414c036af3a3c
Apple <apple@mbp-de-apple.home>
2023-06-16 23:18:41 +0200

    config edit

--- src/main/java/com/kaggle/adult/income/adultincome/AdultIncomeApplication.java
+++ src/main/java/com/kaggle/adult/income/adultincome/AdultIncomeApplication.java
@@ -2,8 +2,10 @@ package com.kaggle.adult.income.adultincome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AdultIncomeApplication {
 
 	public static void main(String[] args) {
