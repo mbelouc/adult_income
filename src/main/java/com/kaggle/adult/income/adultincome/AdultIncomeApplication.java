1854df7a7c3223c83c94ee719a4adfa15b93076f
Apple <apple@mbp-de-apple-5.home>
2023-06-16 16:55:26 +0200

    first commit

---
+++ src/main/java/com/kaggle/adult/income/adultincome/AdultIncomeApplication.java
@@ -0,0 +1,13 @@
+package com.kaggle.adult.income.adultincome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultIncomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdultIncomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaggle/adult/income/adultincome/AdultIncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaggle.adult.income.adultincome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdultIncomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
